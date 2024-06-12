9ef85156703f79a8e30e4650d714db59b4867c51
asana17 <kei17.kaight084@gmail.com>
2024-06-13 00:37:55 +0900

    feat(server): Script to convert Markdown into HTML
    Convert Markdown to HTML, then attach CSS and script.
    
    To use, run "node convert.js <Markdown path> <CSS path> <destination
    html path>"
    
    Signed-off-by: asana17 <kei17.kaight084@gmail.com>

--- node/README.md
+++ node/README.md
@@ -18,23 +18,40 @@ node server.js <port> <directory>
 
 Access `localhost:<port>/<html file>` to see `<html file>` content in `<directory>`.
 
+## `convert.js`
+
+Convert a Markdown file to a HTML file, then add the CSS and the auto-reload script.
+
+To run:
+
+```bash
+node convert.js <markdown path> <css path> <destination html path>
+```
+
 ## Before commit
 
-Use `eslint` and `prettier` for Node.js codes.
-If `eslint` requires the latest Node.js, do:
+- Use `eslint` and `prettier` for Node.js codes.
+  If `eslint` requires the latest Node.js, do:
 
-```bash
-npm install -g n
-n lts
-n latest
-prune
-```
+  ```bash
+  npm install -g n
+  n lts
+  n latest
+  prune
+  ```
 
-These comands can require `sudo`.
+  These comands can require `sudo`.
 
-You can run `eslint` and `prettier` by:
+  You can run `eslint` and `prettier` by:
 
-```bash
-npx eslint .
-npx prettier . --write
-```
+  ```bash
+  npx eslint .
+  npx prettier . --write
+  ```
+
+- Use `stylelint` for CSS files.
+  You can run `stylelint` by:
+
+  ```bash
+  npx stylelint ./config --fix
+  ```
---
+++ node/convert.js
@@ -0,0 +1,66 @@
+// read a markdown file and a css file, and generate an html file with the markdown content
+// html file will automatically reload when updated
+
+const fs = require("fs");
+const { marked } = require("marked");
+const path = require("path");
+
+function generateHtml(markdownFilePath, cssFilePath, callback) {
+  let htmlContent = "<p>empty markdown file...<p>";
+  if (fs.existsSync(markdownFilePath)) {
+    const content = fs.readFileSync(markdownFilePath, "utf8");
+    htmlContent = marked.parse(content);
+  }
+
+  const cssData = fs.readFileSync(cssFilePath, "utf8");
+
+  const fullHtmlContent = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <style>
+  ${cssData}
+  </style
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>${markdownFilePath}</title>
+  <script type="text/javascript" src="https://livejs.com/live.js"></script>
+  <script type="module">
+    import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+    mermaid.initialize({ startOnLoad: true });
+  </script>
+</head>
+<body>
+  ${htmlContent}
+</body>
+</html>`;
+  callback(fullHtmlContent);
+}
+
+function main() {
+  if (process.argv.length !== 5) {
+    console.error(
+      "Usage: node convert.js <markdown path> <css path> <destination path>",
+    );
+    process.exit(1);
+  }
+  const markdownFilePath = process.argv[2];
+  const cssFilePath = process.argv[3];
+  const destFilePath = process.argv[4];
+
+  const destDir = path.dirname(destFilePath);
+  if (!fs.existsSync(destDir)) {
+    console.error(`Destination directory ${destDir} does not exist`);
+    process.exit(1);
+  }
+
+  generateHtml(markdownFilePath, cssFilePath, (html) => {
+    fs.writeFile(destFilePath, html, (err) => {
+      if (err) {
+        console.error(`Error writing file ${destFilePath}:`, err);
+        process.exit(1);
+      }
+    });
+  });
+}
+
+main();
