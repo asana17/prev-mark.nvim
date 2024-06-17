160bae769b365797d33aaa4d871c2eaf2988a8ff
asana17 <kei17.kaight084@gmail.com>
2024-06-18 02:47:22 +0900

    chore(lua/prev-mark): Remove test
    Remove tests since main preview function works as a test.
    
    Signed-off-by: asana17 <kei17.kaight084@gmail.com>

--- lua/prev-mark/init.lua
+++ lua/prev-mark/init.lua
@@ -1,11 +1,9 @@
 local config = require('prev-mark.config')
 local view = require('prev-mark.view')
-local test = require('prev-mark.test')
 
 local M = {}
 
 M.setup = config.setup
-M.run_test = test.run
 M.prev_mark = function()
   view.init()
   view.preview()
