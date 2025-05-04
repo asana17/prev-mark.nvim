10941ab6eb60115c61ea35e85db2b2f1496e93fd
asana17 <kei17.kaight084@gmail.com>
2025-05-05 01:07:28 +0900

    feat(node/convert.js): support latex
    Signed-off-by: asana17 <kei17.kaight084@gmail.com>

--- node/convert.js
+++ node/convert.js
@@ -5,6 +5,49 @@ const fs = require("fs");
 const { marked } = require("marked");
 const path = require("path");
 
+const katexInlineExtension = {
+  name: 'latexInline',
+  level: 'inline',
+  start(src) {
+    return src.indexOf('$');
+  },
+  tokenizer(src) {
+    const match = /^\$([^\$]+?)\$/.exec(src);
+    if (match) {
+      return {
+        type: 'latexInline',
+        raw: match[0],
+        text: match[1],
+        tokens: [],  // required for inline-level tokens
+      };
+    }
+  },
+  renderer(token) {
+    return `<span class="math-inline">\\(${token.text}\\)</span>`;
+  }
+};
+
+const katexBlockExtension = {
+  name: 'latexBlock',
+  level: 'block',
+  start(src) {
+    return src.indexOf('$$');
+  },
+  tokenizer(src) {
+    const match = /^\$\$([\s\S]+?)\$\$/.exec(src);
+    if (match) {
+      return {
+        type: 'latexBlock',
+        raw: match[0],
+        text: match[1].trim(),
+        tokens: [],  // required for block-level tokens
+      };
+    }
+  },
+  renderer(token) {
+    return `<div class="math-display">$$${token.text}$$</div>`;
+  }
+};
 
 const mermaidExtension = {
   name: "mermaid",
@@ -28,7 +71,8 @@ const mermaidExtension = {
   },
 };
 
-marked.use({ extensions: [mermaidExtension] });
+marked.use({
+  extensions: [mermaidExtension, katexInlineExtension, katexBlockExtension] });
 
 function generateHtml(markdownFilePath, cssFilePath, callback) {
   let htmlContent = "<p>empty markdown file...<p>";
@@ -49,13 +93,24 @@ function generateHtml(markdownFilePath, cssFilePath, callback) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>${markdownFilePath}</title>
   <script type="text/javascript" src="https://livejs.com/live.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.min.js"></script>
-  <script>
-    document.addEventListener('DOMContentLoaded', () => {
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.css">
+  <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.js"></script>
+  <script defer src="https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/contrib/auto-render.min.js"></script>
+  <script defer src="https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.min.js"></script>
+  <script defer type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function () {
       mermaid.initialize({ startOnLoad: true });
+      setTimeout(function() {
+        renderMathInElement(document.body, {
+          delimiters: [
+            {left: "$$", right: "$$", display: true},
+            {left: "\\\\(", right: "\\\\)", display: false}
+          ]
+        });
+      }, 100);
     });
   </script>
-</head>
+  </head>
 <body>
   ${htmlContent}
 </body>
